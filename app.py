4cd4e0c3d1b3b9dce1491daebd2ea77abc37d057
Quentin Torroba <torroba.q@gmail.com>
2023-02-18 18:17:17 +0100

    refactor: Moved all stablediffusion to core

---
+++ app.py
@@ -0,0 +1,32 @@
+from core.scripts.txt2imgapi import generate
+from fastapi import FastAPI
+from fastapi.responses import Response
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Prompt(BaseModel):
+    prompt: str
+    negative_prompt: str
+    height: int = 512
+    width: int = 512
+    model: str = "sdv1/1-5.safetensors"
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.post(
+    "/txt2image", 
+    response_class=Response,
+    responses = {
+        200: {
+            "content": {"image/png": {}}
+        }
+    }
+
+)
+def generateImage(prompt: Prompt):
+    image_bytes: bytes = generate(prompt=prompt.prompt,negative_prompt=prompt.negative_prompt,W=prompt.width, H=prompt.height, ckpt="core/ldm/models/" + prompt.model,)
+    # Return the image in the response
+    return Response(content=image_bytes, media_type="image/png")
