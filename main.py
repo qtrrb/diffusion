467e6fcd9d400dbb412a9aef13a22e071a75d04c
Quentin Torroba <torroba.q@gmail.com>
2023-02-26 23:05:45 +0100

    Renamed app to main

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from core.scripts.txt2imgapi import generate
 from core.scripts.img2imgapi import generate_from_image
 from fastapi import FastAPI, HTTPException
@@ -5,6 +6,7 @@ from fastapi.responses import Response
 from pydantic import BaseModel
 
 app = FastAPI()
+
 
 class Prompt(BaseModel):
     prompt: str
@@ -14,6 +16,7 @@ class Prompt(BaseModel):
     model: str = "sdv1/1-5.safetensors"
     vae: str = ""
 
+
 class ImagePrompt(BaseModel):
     prompt: str
     negative_prompt: str
@@ -21,14 +24,16 @@ class ImagePrompt(BaseModel):
     model: str = "sdv1/1-5.safetensors"
     vae: str = ""
 
+
 @app.get("/")
 def read_root():
     return {"Status": "API running"}
 
+
 @app.post(
-    "/txt2img", 
+    "/txt2img",
     response_class=Response,
-    responses = {
+    responses={
         200: {
             "content": {"image/png": {}}
         }
@@ -37,18 +42,19 @@ def read_root():
 )
 def txt2img(prompt: Prompt):
     try:
-        image_bytes: bytes = generate(prompt=prompt.prompt,negative_prompt=prompt.negative_prompt,W=prompt.width, H=prompt.height, ckpt=prompt.model, vae=prompt.vae)
+        image_bytes: bytes = generate(prompt=prompt.prompt, negative_prompt=prompt.negative_prompt,
+                                      W=prompt.width, H=prompt.height, ckpt=prompt.model, vae=prompt.vae)
         # Return the image in the response
         return Response(content=image_bytes, media_type="image/png")
     except Exception as e:
-            print(e)
-            raise HTTPException(status_code=500, detail="Error generating image")
+        print(e)
+        raise HTTPException(status_code=500, detail="Error generating image")
 
 
 @app.post(
-    "/img2img", 
+    "/img2img",
     response_class=Response,
-    responses = {
+    responses={
         200: {
             "content": {"image/png": {}}
         }
@@ -57,9 +63,14 @@ def txt2img(prompt: Prompt):
 )
 def img2img(prompt: ImagePrompt):
     try:
-        image_bytes: bytes = generate_from_image(prompt=prompt.prompt,negative_prompt=prompt.negative_prompt,init_img_url=prompt.image, ckpt=prompt.model, vae=prompt.vae)
+        image_bytes: bytes = generate_from_image(
+            prompt=prompt.prompt, negative_prompt=prompt.negative_prompt, init_img_url=prompt.image, ckpt=prompt.model, vae=prompt.vae)
         # Return the image in the response
         return Response(content=image_bytes, media_type="image/png")
     except Exception as e:
         print(e)
         raise HTTPException(status_code=500, detail="Error generating image")
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000, log_level="info")
